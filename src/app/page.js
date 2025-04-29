52395a825d92c1f9ef7c981c670c2cd47de4c55c
mjjstockman <mjjstockman@gmail.com>
2025-04-29 01:40:17 +0100

    initial setup

---
+++ src/app/page.js
@@ -0,0 +1,28 @@
+'use client'; // if you're inside app/ directory (required for client-side code)
+
+import { supabase } from '@/lib/supabaseClient';
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data, error } = await supabase
+        .from('test') //
+        .select('*');
+
+      if (error) console.error('Supabase error:', error);
+      else setData(data);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <main className='p-10'>
+      <h1 className='text-2xl font-bold'>Supabase Test</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </main>
+  );
+}
